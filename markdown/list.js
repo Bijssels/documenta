446e7064746d5638a9e6353528210ffe7002816c
Bijssels <bram.ijssels@gmail.com>
2025-05-22 22:00:35 +0200

    Update list.js

--- markdown/list.js
+++ markdown/list.js
@@ -1,16 +1,46 @@
 export default function(content) {
-    let edited = false;
-  
-    content = content.replace(/(^|\n)[*\-+]\s+(.+)/g, (match, prefix, text) => {
-      edited = true;
-      return `${prefix}<ul><li>${text}</li></ul>`;
-    });
-  
-    content = content.replace(/(^|\n)(\d+)\.\s+(.+)/g, (match, prefix, number, text) => {
-      edited = true;
-      return `${prefix}<ol><li>${text}</li></ol>`;
-    });
-  
-    return edited ? content : false;
+  let lines = content.split('\n');
+  let result = [];
+  let inUl = false;
+  let inOl = false;
+  let edited = false;
+
+  for (let i = 0; i < lines.length; i++) {
+    let line = lines[i];
+
+    let ulMatch = line.match(/^\s*[*\-+]\s+(.+)/);
+    let olMatch = line.match(/^\s*(\d+)\.\s+(.+)/);
+
+    if (ulMatch) {
+      if (!inUl) {
+        result.push('<ul>');
+        inUl = true;
+        edited = true;
+      }
+      result.push(`<li>${ulMatch[1]}</li>`);
+    } else if (olMatch) {
+      if (!inOl) {
+        result.push('<ol>');
+        inOl = true;
+        edited = true;
+      }
+      result.push(`<li>${olMatch[2]}</li>`);
+    } else {
+      if (inUl) {
+        result.push('</ul>');
+        inUl = false;
+      }
+      if (inOl) {
+        result.push('</ol>');
+        inOl = false;
+      }
+      result.push(line);
+    }
   }
   
+  if (inUl) result.push('</ul>');
+  if (inOl) result.push('</ol>');
+
+  let newContent = result.join('\n');
+  return edited ? newContent : false;
+}
